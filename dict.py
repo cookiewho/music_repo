342a619d15ce9fe66ca81fb1f722246e42dd8252
David  Gaeta <dgaeta@csumb.edu>
2018-08-02 21:45:43 +0000

    dict.py

--- dict.py
+++ dict.py
@@ -1,4 +1,7 @@
-song = {"artist": "Drake",}
+song = {
+    "artist": "Florence and the Machines",
+    "song_name":"God's Plan"
+}
 def print_dict(dict):
     for key in dict:
         print dict[key]
